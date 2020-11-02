29984701c61b33645f666d003e943b4af0ded1c8
Sarthak Khandelwal <sarthakk5678@gmail.com>
2020-11-03 00:09:02 +0530

    commit

---
+++ C++ Programs/06-print all natural numbers in n to 1 in reverse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int num=0, i;
+    cout<<"Enter any number: "<<endl;
+    cin>>num;
+    for(i=num; i>=1; i--)
+    {
+        cout<<i;
+    }
+    return 0;
+ 
+}
